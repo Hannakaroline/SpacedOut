7534fcb4016a02d0c5371a83606d5fbbbdd46286
Hannakaroline <karolinepalacios@gmail.com>
2025-02-15 17:31:27 +0900

    project iniciated

---
+++ SpacedOut/SpacedOutApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpacedOutApp.swift
+//  SpacedOut
+//
+//  Created by Hanna Torales Palacios on 2025/02/15.
+//
+
+import SwiftUI
+
+@main
+struct SpacedOutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
