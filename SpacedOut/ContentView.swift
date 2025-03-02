a15100e3ff69d49f206d0628d615b1da2469ca41
Hannakaroline <karolinepalacios@gmail.com>
2025-03-02 13:37:07 +0900

    categories list

--- SpacedOut/ContentView.swift
+++ SpacedOut/ContentView.swift
@@ -8,14 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    @EnvironmentObject var dataController: DataController
+    @Environment(\.managedObjectContext) var managedObjectContext
+    
+    @FetchRequest(sortDescriptors: [SortDescriptor(\Category.name), SortDescriptor(\Category.creationDate)], predicate: NSPredicate(format: "active = true")) var activeCategories
+    @FetchRequest(sortDescriptors: [SortDescriptor(\Category.name), SortDescriptor(\Category.creationDate)], predicate: NSPredicate(format: "active = false")) var inactiveCategories
+
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                if activeCategories.isEmpty == false {
+                    Section("Active Categories") {
+                        ForEach(activeCategories, content: CategoryRow.init)
+                    }
+                }
+                
+                Section("Inactive Categories") {
+                    if inactiveCategories.isEmpty {
+                        Text("none")
+                            .foregroundStyle(.secondary)
+                    } else {
+                        ForEach(inactiveCategories, content: CategoryRow.init)
+                    }
+                }
+            }
+            .navigationTitle("Categories")
+            .navigationDestination(for: Category.self) { category in
+                CategoryView(category: category)
+                
+            }
+            .toolbar {
+                Button(action: newCategory) {
+                    Label("Create new category", systemImage: "plus")
+                }
+            }
         }
-        .padding()
+    }
+    
+    func newCategory() {
+        withAnimation {
+            let category = Category(context: managedObjectContext)
+            category.name = "New Category"
+            category.creationDate = .now
+            category.active = false
+            dataController.save()
+        }
     }
 }
 
